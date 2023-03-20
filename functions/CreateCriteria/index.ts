d67b1024e4ee5f8f2ba6c006d16f5479332cce01
Sámuel Fekete <feketesamu@gmail.com>
2023-03-20 22:03:44 +0100

    test

--- functions/CreateCriteria/index.ts
+++ functions/CreateCriteria/index.ts
@@ -2,13 +2,14 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import prisma from "../lib/prisma";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log(prisma)
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     try {
-
+        //const criteria = await prisma.criteria.create({data: {name, description: 'Az első szempont', minValue: 0, maxValue: 3, weight: 100}})
         context.log('Query executed!')
         context.res = {
-            body: 'alma'
+            body: name
         };
     } catch (e) {
         context.log(e)
