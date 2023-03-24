1d2351313dbeb5dbc84d119f9217c5337ab73f00
Sámuel Fekete <feketesamu@gmail.com>
2023-03-24 22:26:15 +0100

    prisma log

--- functions/lib/prisma.ts
+++ functions/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 console.log("Starting to init prisma...")
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({log: [{emit: 'stdout', level: 'info'}, {emit: 'stdout', level: 'warn'}, {emit: 'stdout', level: 'error'}]})
 console.log("prisma initilaized...")
 prisma.$connect().then(() => console.log("Connected to db!"))
 
