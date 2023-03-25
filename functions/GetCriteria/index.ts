b4017dd1c769d5c3b7fcd319f95f0b1263523473
Sámuel Fekete <feketesamu@gmail.com>
2023-03-25 23:07:48 +0100

    fds

--- functions/GetCriteria/index.ts
+++ functions/GetCriteria/index.ts
@@ -4,8 +4,8 @@ import {connect, query, config} from 'mssql'
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     try {
-        const configAzure: config = {user: 'dbadmin', password: process.env.DATABASE_URL, database: 'pontozo-db', server: 'pontozo-db-server.database.windows.net',port: 1433, options: {trustServerCertificate: true}}
-        const configLocal: config = {user: 'sa', password: "Pr1sm4_Pr1sm4", database: 'pontozo-db', server: 'localhost',port: 1433, options: {trustServerCertificate: true}}
+        //const configAzure: config = {user: 'dbadmin', password: process.env.DATABASE_URL, database: 'pontozo-db', server: 'pontozo-db-server.database.windows.net',port: 1433, options: {trustServerCertificate: true}}
+        const configLocal: config = {user: process.env.DB_USER, password: process.env.DB_PWD, database: 'pontozo-db', server: process.env.DB_SERVER,port: 1433, options: {trustServerCertificate: true}}
         await connect(configLocal)
         const b = await query("SELECT * FROM Criteria;")
         context.log('Query executed!')
