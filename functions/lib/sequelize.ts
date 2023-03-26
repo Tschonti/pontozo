310024020de5e5b22266bbe5529b0bb20d9a1098
Sámuel Fekete <feketesamu@gmail.com>
2023-03-26 14:01:07 +0200

    seq connection fix

--- functions/lib/sequelize.ts
+++ functions/lib/sequelize.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const seq = new Sequelize(process.env.DATABASE_URL)
+const seq = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, { dialect: 'mssql', host: process.env.DB_SERVER })
 
 export default seq
