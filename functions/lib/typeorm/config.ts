d334796444259267795b9d2000ab6feb5329c090
Sámuel Fekete <feketesamu@gmail.com>
2023-03-28 22:14:14 +0200

    encrypt

--- functions/lib/typeorm/config.ts
+++ functions/lib/typeorm/config.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { DB_NAME, DB_PWD, DB_SERVER, DB_USER } from "../env"
+import { DB_NAME, DB_PWD, DB_SERVER, DB_USER, ENCRYPT } from "../env"
 import Criterion from "./entities/Criterion"
 import Rating from "./entities/Rating"
 import { init1679948629543 } from "./migrations/1679948629543-init"
@@ -18,7 +18,7 @@ export const AppDataSource = new DataSource({
     entities: [Criterion, Rating],
     subscribers: [],
     migrations: [init1679948629543, rating1679949145989],
-    options: {encrypt: false},
+    options: {encrypt: ENCRYPT},
 })
 
 AppDataSource.initialize()
