da52c1e4da3d53db406ff73b616a77bbac0c2f0a
Sámuel Fekete <feketesamu@gmail.com>
2023-04-28 22:59:33 +0200

    client id to workflow

--- client/src/util/environment.ts
+++ client/src/util/environment.ts
@@ -4,4 +4,3 @@ export const FUNC_HOST = env.get('REACT_APP_FUNC_HOST').default('http://localhos
 export const APIM_HOST = env.get('REACT_APP_APIM_HOST').default('https://pontozo-apim.azure-api.net').asString()
 export const APIM_KEY = env.get('REACT_APP_APIM_KEY').required().asString()
 export const CLIENT_ID = env.get('REACT_APP_CLIENT_ID').default('').asString()
-export const CLIENT_SECRET = env.get('REACT_APP_CLIENT_SECRET').default('').asString()
