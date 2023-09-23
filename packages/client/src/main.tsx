f2d49c6696275c717faa18c8a7464ecb49e560c1
Sámuel Fekete <feketesamu@gmail.com>
2023-09-23 18:57:24 +0200

    application insights connected

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -6,8 +8,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './api/contexts/AuthContext'
 import { RatingProvider } from './api/contexts/RatingProvider'
 import { App } from './App'
+import { APP_INSIGHTS_CONN_STR } from './util/environment'
 import { queryClient } from './util/queryClient'
 import theme from './util/theme'
+
+const reactPlugin = new ReactPlugin()
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: APP_INSIGHTS_CONN_STR,
+    extensions: [reactPlugin],
+    enableAutoRouteTracking: true,
+  },
+})
+appInsights.loadAppInsights()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
--- packages/client/src/util/environment.ts
+++ packages/client/src/util/environment.ts
@@ -2,3 +2,4 @@ export const FUNC_HOST = import.meta.env.VITE_FUNC_HOST || 'http://localhost:300
 export const APIM_HOST = import.meta.env.VITE_APIM_HOST || 'https://pontozoapim.azure-api.net'
 export const APIM_KEY = import.meta.env.VITE_APIM_KEY
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+export const APP_INSIGHTS_CONN_STR = import.meta.env.VITE_APP_INSIGHTS_CONN_STR
