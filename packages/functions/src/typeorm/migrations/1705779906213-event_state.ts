68b5d83151ac46e2bec4d41ffa5a35dcf616a0be
Sámuel Fekete <feketesamu@gmail.com>
2024-03-29 16:00:04 +0100

    fix db name in migration

--- packages/functions/src/typeorm/migrations/1705779906213-event_state.ts
+++ packages/functions/src/typeorm/migrations/1705779906213-event_state.ts
@@ -4,7 +4,7 @@ export class EventState1705779906213 implements MigrationInterface {
   name = 'EventState1705779906213'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`EXEC sp_rename "pontozo-db.dbo.event.rateable", "state"`)
+    await queryRunner.query(`EXEC sp_rename "pontozo-dtu-db.dbo.event.rateable", "state"`)
     await queryRunner.query(`ALTER TABLE "event" DROP CONSTRAINT "DF_60718ea2c6e6abd42b982dd321a"`)
     await queryRunner.query(`ALTER TABLE "event" ADD CONSTRAINT "DF_5bcb4691305a70a213f794665c2" DEFAULT 1 FOR "state"`)
     await queryRunner.query(`ALTER TABLE "event" DROP CONSTRAINT "DF_5bcb4691305a70a213f794665c2"`)
@@ -25,6 +25,6 @@ export class EventState1705779906213 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "event" ADD CONSTRAINT "DF_5bcb4691305a70a213f794665c2" DEFAULT 1 FOR "state"`)
     await queryRunner.query(`ALTER TABLE "event" DROP CONSTRAINT "DF_5bcb4691305a70a213f794665c2"`)
     await queryRunner.query(`ALTER TABLE "event" ADD CONSTRAINT "DF_60718ea2c6e6abd42b982dd321a" DEFAULT 1 FOR "state"`)
-    await queryRunner.query(`EXEC sp_rename "pontozo-db.dbo.event.state", "rateable"`)
+    await queryRunner.query(`EXEC sp_rename "pontozo-dtu-db.dbo.event.state", "rateable"`)
   }
 }
