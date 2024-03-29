8462e881e17098c38c18cfbc4533616852e4b0bb
Sámuel Fekete <feketesamu@gmail.com>
2024-03-29 15:48:12 +0100

    remove comment, format migrations

--- packages/functions/src/functions/auth/login.ts
+++ packages/functions/src/functions/auth/login.ts
@@ -17,17 +17,12 @@ export const login = async (req: HttpRequest, context: InvocationContext): Promi
 
     const oauthToken = await getToken(authorizationCode)
     const user = await getUser(oauthToken.access_token)
-    // const dataSource = await Promise.race([getAppDataSource(context), getRedisClient(context)])
     const dataSource = await getAppDataSource(context)
     let roles = []
     if (dataSource instanceof DataSource) {
       context.log('Logging in from DB')
       roles = (await dataSource.getRepository(UserRoleAssignment).find({ where: { userId: user.szemely_id } })).map((r) => r.role)
-    } /*else {
-      context.log('Logging in from cache')
-      const rawRoles = await dataSource.hGetAll(`ura:${user.szemely_id}`)
-      roles = rawRoles ? Object.values(rawRoles) : []
-    }*/
+    }
 
     const jwtToken = jwt.sign({ ...user, roles }, JWT_SECRET, { expiresIn: '2 days' })
     context.log(`User #${user.szemely_id} signed in`)
--- packages/functions/src/typeorm/migrations/1705141629515-message_to_rating.ts
+++ packages/functions/src/typeorm/migrations/1705141629515-message_to_rating.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class MessageToRating1705141629515 implements MigrationInterface {
-    name = 'MessageToRating1705141629515'
+  name = 'MessageToRating1705141629515'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event_rating" ADD "message" nvarchar(255)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "event_rating" ADD "message" nvarchar(255)`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event_rating" DROP COLUMN "message"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "event_rating" DROP COLUMN "message"`)
+  }
 }
