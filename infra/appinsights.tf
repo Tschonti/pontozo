5fa005775416b303c2594ee348e5e88b5b6e395f
Sámuel Fekete <feketesamu@gmail.com>
2024-03-30 22:29:29 +0100

    add log analytics ws to terraform

--- infra/appinsights.tf
+++ infra/appinsights.tf
@@ -1,7 +1,16 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "pontozo-law-tf"
+  location            = azurerm_resource_group.tf-rg.location
+  resource_group_name = azurerm_resource_group.tf-rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_application_insights" "appinsights" {
   name                = "pontozo-insights-tf"
   location            = azurerm_resource_group.tf-rg.location
   resource_group_name = azurerm_resource_group.tf-rg.name
+  workspace_id        = azurerm_log_analytics_workspace.law.id
   application_type    = "web"
 }
 
