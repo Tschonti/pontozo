f60b0e3bdbf03b955ddb63bb32906e9e5359b775
Sámuel Fekete <feketesamu@gmail.com>
2024-04-29 18:46:38 +0200

    add envvars to fa and fa deploy job

--- infra/functionapp.tf
+++ infra/functionapp.tf
@@ -1,3 +1,22 @@
+variable "FA_ADMINS" {
+  type      = string
+  sensitive = true
+}
+
+variable "FA_FUNCTIONS_URL" {
+  type = string
+}
+
+variable "DB_PWD" {
+  type      = string
+  sensitive = true
+}
+
+variable "FA_JWT_SECRET" {
+  type      = string
+  sensitive = true
+}
+
 resource "azurerm_service_plan" "sp" {
   name                = "pontozo-sp-tf"
   resource_group_name = azurerm_resource_group.tf-rg.name
@@ -27,5 +46,29 @@ resource "azurerm_windows_function_app" "function-app" {
     cors {
       allowed_origins = ["https://pontozo.mtfsz.hu"]
     }
+  }
+
+  app_settings = {
+    "ADMINS"        = var.FA_ADMINS
+    "APIM_HOST"     = azurerm_api_management.apim.gateway_url
+    "APIM_KEY"      = ""
+    "CLIENT_ID"     = var.MTFSZ_CLIENT_ID
+    "CLIENT_SECRET" = var.MTFSZ_CLIENT_SECRET
+    # "DB_ADMIN_PWD" = ""
+    # "DB_ADMIN_USER" = ""
+    "DB_NAME"                  = azurerm_mssql_database.sqldatabase.sqldatabase.name
+    "DB_PWD"                   = var.DB_PWD
+    "DB_SERVER"                = azurerm_mssql_server.sqlserver.fully_qualified_domain_name
+    "DB_USER"                  = var.DB_PWD
+    "ENCRYPT"                  = true
+    "FRONTEND_URL"             = "TODO"
+    "FUNCTION_HOST"            = var.FA_FUNCTIONS_URL
+    "FUNCTIONS_WORKER_RUNTIME" = "node"
+    "JWT_SECRET"               = var.FA_JWT_SECRET
+    "REDIS_HOST"               = "TODO"
+    "REDIS_PORT"               = "TODO"
+    "REDIS_PWD"                = "TODO"
+    "WEBSITE_RUN_FROM_PACKAGE" = 1
+    "WEBSITE_TIME_ZONE"        = "Central Europe Standard Time"
   }
 }
