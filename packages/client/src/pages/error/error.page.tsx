f0f4571f93f83b5b2407274ee20dab645f23601a
Sámuel Fekete <feketesamu@gmail.com>
2024-06-15 21:34:47 +0200

    fix retry button on error page

--- packages/client/src/pages/error/error.page.tsx
+++ packages/client/src/pages/error/error.page.tsx
@@ -1,13 +1,15 @@
 import { Button, Heading, Text, VStack } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 import { HelmetTitle } from 'src/components/commons/HelmetTitle'
 
 export const ErrorPage = () => {
+  const nav = useNavigate()
   return (
     <VStack w="100%" alignItems="center" my={2}>
       <HelmetTitle title="Pontoz-O | Hiba" />
       <Heading>Ismeretlen hiba</Heading>
       <Text>{'Kérlek próbáld újra, és ha a hiba nem hárul el, jelezd a fejlesztőnek a feketesamu{kukac}gmail{pont}hu címen!'}</Text>
-      <Button onClick={() => window.location.reload()} colorScheme="brand">
+      <Button onClick={() => nav(-2)} colorScheme="brand">
         Újrapróbálom
       </Button>
     </VStack>
