af0500f0cb55bdc830ae2cb4740abc44e73fc33c
Sámuel Fekete <feketesamu@gmail.com>
2024-08-18 13:52:23 +0200

    drawer refactor, min 1 crit

--- packages/client/src/pages/results/Results.page.tsx
+++ packages/client/src/pages/results/Results.page.tsx
@@ -1,48 +1,25 @@
-import {
-  Button,
-  Checkbox,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  FormLabel,
-  Heading,
-  Select,
-  Stack,
-  Text,
-  useDisclosure,
-  useToast,
-  VStack,
-} from '@chakra-ui/react'
+import { Checkbox, FormLabel, Heading, Select, Stack, VStack } from '@chakra-ui/react'
 import { AgeGroup, ALL_AGE_GROUPS, ALL_ROLES, RatingRole } from '@pontozo/common'
-import { ChangeEvent, Fragment, useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { useFetchEventResultsMutation, useFetchSeasonsMutation } from 'src/api/hooks/resultHooks'
 import { HelmetTitle } from 'src/components/commons/HelmetTitle'
 import { NavigateWithError } from 'src/components/commons/NavigateWithError'
 import { PATHS } from 'src/util/paths'
 import { LoadingSpinner } from '../../components/commons/LoadingSpinner'
 import { AgeGroupRoleSelector } from './components/AgeGroupRoleSelector'
+import { CriteriaDrawer } from './components/CriteriaDrawer'
 import { EventResultTable } from './components/EventResultTable'
 
 export const ResultsPage = () => {
   const resultsMutation = useFetchEventResultsMutation()
   const seasonsMutation = useFetchSeasonsMutation()
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const [selectedCriterionIds, setSelectedCriterionIds] = useState<number[]>([])
   const [selectedCategoryIds, setSelectedCategoryIds] = useState<number[]>([])
-  const [storedCriterionIds, setStoredCriterionIds] = useState<number[]>([])
-  const [storedCategoryIds, setStoredCategoryIds] = useState<number[]>([])
   const [selectedSeasonId, setSelectedSeasonId] = useState<number>()
   const [selectedRoles, setSelectedRoles] = useState<RatingRole[]>(ALL_ROLES)
   const [selectedAgeGroups, setSelectedAgeGroups] = useState<AgeGroup[]>(ALL_AGE_GROUPS)
   const [nationalOnly, setNationalOnly] = useState(false)
   const [includeTotal, setIncludeTotal] = useState(true)
-  const [storedIncludeTotal, setStoredIncludeTotal] = useState(true)
-  const selectCritCount = selectedCategoryIds.length + selectedCriterionIds.length + (includeTotal ? 1 : 0)
-  const toast = useToast()
 
   useEffect(() => {
     seasonsMutation.mutate(undefined)
@@ -56,31 +33,6 @@ export const ResultsPage = () => {
     return <NavigateWithError to={PATHS.ERROR} error={resultsMutation.error ?? seasonsMutation.error!} />
   }
 
-  const showToast = () => {
-    const toastId = 'max-crit-reached-warning'
-    if (!toast.isActive(toastId)) {
-      toast({ id: toastId, title: 'Maximum 5 szempont jeleníthető meg egyszerre!', status: 'warning' })
-    }
-  }
-
-  const categoryCheckChange = (event: ChangeEvent, categoryId: number) => {
-    if ((event.target as HTMLInputElement).checked) {
-      if (selectCritCount > 4) return showToast()
-      setSelectedCategoryIds([...selectedCategoryIds, categoryId])
-    } else {
-      setSelectedCategoryIds(selectedCategoryIds.filter((cId) => cId !== categoryId))
-    }
-  }
-
-  const criterionCheckChange = (event: ChangeEvent, criterrionId: number) => {
-    if ((event.target as HTMLInputElement).checked) {
-      if (selectCritCount > 4) return showToast()
-      setSelectedCriterionIds([...selectedCriterionIds, criterrionId])
-    } else {
-      setSelectedCriterionIds(selectedCriterionIds.filter((cId) => cId !== criterrionId))
-    }
-  }
-
   const sendResultRequest = (national?: boolean) => {
     resultsMutation.mutate({
       seasonId: selectedSeasonId,
@@ -89,11 +41,6 @@ export const ResultsPage = () => {
       nationalOnly: national ?? nationalOnly,
       includeTotal,
     })
-  }
-
-  const saveSelectedIds = () => {
-    sendResultRequest()
-    onClose()
   }
 
   const selectedSeasonChange = (event: ChangeEvent) => {
@@ -122,20 +69,6 @@ export const ResultsPage = () => {
     sendResultRequest((event.target as HTMLInputElement).checked)
   }
 
-  const drawerOpened = () => {
-    setStoredCategoryIds(selectedCategoryIds)
-    setStoredCriterionIds(selectedCriterionIds)
-    setStoredIncludeTotal(includeTotal)
-    onOpen()
-  }
-
-  const drawerDismissed = () => {
-    setSelectedCategoryIds(storedCategoryIds)
-    setSelectedCriterionIds(storedCriterionIds)
-    setIncludeTotal(storedIncludeTotal)
-    onClose()
-  }
-
   return (
     <>
       <HelmetTitle title="Pontoz-O" />
@@ -161,61 +94,17 @@ export const ResultsPage = () => {
         <Checkbox colorScheme="brand" isChecked={nationalOnly} onChange={nationalOnlyChange}>
           Csak országos és kiemelt rangsoroló versenyek
         </Checkbox>
-        <Button colorScheme="brand" onClick={drawerOpened}>
-          Szempontok ({selectCritCount})
-        </Button>
+        <CriteriaDrawer
+          includeTotal={includeTotal}
+          setIncludeTotal={setIncludeTotal}
+          selectedCriterionIds={selectedCriterionIds}
+          setSelectedCriterionIds={setSelectedCriterionIds}
+          selectedCategoryIds={selectedCategoryIds}
+          setSelectedCategoryIds={setSelectedCategoryIds}
+          selectedSeason={seasonsMutation.data?.selectedSeason}
+          onSave={sendResultRequest}
+        />
       </Stack>
-      <Drawer isOpen={isOpen} placement="right" onClose={drawerDismissed} size="md">
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Megjelenített szempontok</DrawerHeader>
-
-          <DrawerBody>
-            <VStack alignItems="flex-start" gap={1}>
-              <Text mb={2}>
-                Itt választhatod ki, mely szempontok és kategóriák szerint szeretnéd megtekinteni az értékelés eredményeit. A kategóriáknál
-                a hozzá tartozó szempontok átlagai, az 'Összesített átlag' esetében pedig minden szempont átlaga fog megjelenni.
-              </Text>
-              <Checkbox colorScheme="brand" isChecked={includeTotal} onChange={() => setIncludeTotal(!includeTotal)}>
-                <Text fontWeight="bold">Összesített átlag</Text>
-              </Checkbox>
-              {seasonsMutation.data?.selectedSeason.categories.map((c) => (
-                <Fragment key={c.id}>
-                  <Checkbox
-                    colorScheme="brand"
-                    isChecked={selectedCategoryIds.includes(c.id)}
-                    onChange={(e) => categoryCheckChange(e, c.id)}
-                  >
-                    <Text fontWeight="bold">{c.name}</Text>
-                  </Checkbox>
-                  <VStack alignItems="flex-start" gap={1} ml={4}>
-                    {c.criteria.map((cc) => (
-                      <Checkbox
-                        colorScheme="brand"
-                        key={cc.id}
-                        isChecked={selectedCriterionIds.includes(cc.id)}
-                        onChange={(e) => criterionCheckChange(e, cc.id)}
-                      >
-                        {cc.name}
-                      </Checkbox>
-                    ))}
-                  </VStack>
-                </Fragment>
-              ))}
-            </VStack>
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button colorScheme="gray" mr={3} onClick={drawerDismissed}>
-              Mégse
-            </Button>
-            <Button colorScheme="brand" onClick={saveSelectedIds}>
-              Alkalmaz
-            </Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
       {!resultsMutation.data || resultsMutation.isLoading ? (
         <LoadingSpinner />
       ) : (
---
+++ packages/client/src/pages/results/components/CriteriaDrawer.tsx
@@ -0,0 +1,161 @@
+import {
+  Button,
+  Checkbox,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Text,
+  useDisclosure,
+  useToast,
+  VStack,
+} from '@chakra-ui/react'
+import { SeasonWithEverything } from '@pontozo/common'
+import { ChangeEvent, Fragment, useState } from 'react'
+
+type Props = {
+  selectedCriterionIds: number[]
+  setSelectedCriterionIds: (newIds: number[]) => void
+  selectedCategoryIds: number[]
+  setSelectedCategoryIds: (newIds: number[]) => void
+  includeTotal: boolean
+  setIncludeTotal: (newValue: boolean) => void
+  onSave: () => void
+  selectedSeason?: SeasonWithEverything
+}
+
+const maxCritToastId = 'max-crit-reached-warning'
+const maxCritToastTitle = 'Maximum 5 szempont jeleníthető meg egyszerre!'
+
+const minCritToastId = 'min-crit-reached-warning'
+const minCritToastTitle = 'Minimum 1 szempontot ki kell választanod!'
+
+export const CriteriaDrawer = ({
+  includeTotal,
+  selectedCategoryIds,
+  selectedCriterionIds,
+  onSave,
+  selectedSeason,
+  setSelectedCategoryIds,
+  setSelectedCriterionIds,
+  setIncludeTotal,
+}: Props) => {
+  const [storedCriterionIds, setStoredCriterionIds] = useState<number[]>([])
+  const [storedCategoryIds, setStoredCategoryIds] = useState<number[]>([])
+  const [storedIncludeTotal, setStoredIncludeTotal] = useState(true)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+  const selectCritCount = selectedCategoryIds.length + selectedCriterionIds.length + (includeTotal ? 1 : 0)
+
+  const saveSelectedIds = () => {
+    onSave()
+    onClose()
+  }
+
+  const showToast = (toastId: string, toastTitle: string) => {
+    if (!toast.isActive(toastId)) {
+      toast({ id: toastId, title: toastTitle, status: 'warning' })
+    }
+  }
+
+  const categoryCheckChange = (event: ChangeEvent, categoryId: number) => {
+    if ((event.target as HTMLInputElement).checked) {
+      if (selectCritCount > 4) return showToast(maxCritToastId, maxCritToastTitle)
+      setSelectedCategoryIds([...selectedCategoryIds, categoryId])
+    } else {
+      if (selectCritCount === 1) return showToast(minCritToastId, minCritToastTitle)
+      setSelectedCategoryIds(selectedCategoryIds.filter((cId) => cId !== categoryId))
+    }
+  }
+
+  const criterionCheckChange = (event: ChangeEvent, criterrionId: number) => {
+    if ((event.target as HTMLInputElement).checked) {
+      if (selectCritCount > 4) return showToast(maxCritToastId, maxCritToastTitle)
+      setSelectedCriterionIds([...selectedCriterionIds, criterrionId])
+    } else {
+      if (selectCritCount === 1) return showToast(minCritToastId, minCritToastTitle)
+      setSelectedCriterionIds(selectedCriterionIds.filter((cId) => cId !== criterrionId))
+    }
+  }
+
+  const includeTotalChange = () => {
+    if (includeTotal && selectCritCount === 1) return showToast(minCritToastId, minCritToastTitle)
+    setIncludeTotal(!includeTotal)
+  }
+
+  const drawerOpened = () => {
+    setStoredCategoryIds(selectedCategoryIds)
+    setStoredCriterionIds(selectedCriterionIds)
+    setStoredIncludeTotal(includeTotal)
+    onOpen()
+  }
+
+  const drawerDismissed = () => {
+    setSelectedCategoryIds(storedCategoryIds)
+    setSelectedCriterionIds(storedCriterionIds)
+    setIncludeTotal(storedIncludeTotal)
+    onClose()
+  }
+
+  return (
+    <>
+      <Button colorScheme="brand" onClick={drawerOpened}>
+        Szempontok ({selectCritCount})
+      </Button>
+      <Drawer isOpen={isOpen} placement="right" onClose={drawerDismissed} size="md">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Megjelenített szempontok</DrawerHeader>
+
+          <DrawerBody>
+            <VStack alignItems="flex-start" gap={1}>
+              <Text mb={2}>
+                Itt választhatod ki, mely szempontok és kategóriák szerint szeretnéd megtekinteni az értékelés eredményeit. A kategóriáknál
+                a hozzá tartozó szempontok átlagai, az 'Összesített átlag' esetében pedig minden szempont átlaga fog megjelenni.
+              </Text>
+              <Checkbox colorScheme="brand" isChecked={includeTotal} onChange={includeTotalChange}>
+                <Text fontWeight="bold">Összesített átlag</Text>
+              </Checkbox>
+              {selectedSeason?.categories.map((c) => (
+                <Fragment key={c.id}>
+                  <Checkbox
+                    colorScheme="brand"
+                    isChecked={selectedCategoryIds.includes(c.id)}
+                    onChange={(e) => categoryCheckChange(e, c.id)}
+                  >
+                    <Text fontWeight="bold">{c.name}</Text>
+                  </Checkbox>
+                  <VStack alignItems="flex-start" gap={1} ml={4}>
+                    {c.criteria.map((cc) => (
+                      <Checkbox
+                        colorScheme="brand"
+                        key={cc.id}
+                        isChecked={selectedCriterionIds.includes(cc.id)}
+                        onChange={(e) => criterionCheckChange(e, cc.id)}
+                      >
+                        {cc.name}
+                      </Checkbox>
+                    ))}
+                  </VStack>
+                </Fragment>
+              ))}
+            </VStack>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button colorScheme="gray" mr={3} onClick={drawerDismissed}>
+              Mégse
+            </Button>
+            <Button colorScheme="brand" onClick={saveSelectedIds}>
+              Alkalmaz
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
