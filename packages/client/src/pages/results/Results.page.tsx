6afb95ae1207a243d43aa26cbc501135bd4ea0ae
Sámuel Fekete <feketesamu@gmail.com>
2024-08-20 17:43:22 +0200

    save filter to localStorage

--- packages/client/src/pages/results/Results.page.tsx
+++ packages/client/src/pages/results/Results.page.tsx
@@ -9,6 +9,9 @@ import { LoadingSpinner } from '../../components/commons/LoadingSpinner'
 import { AgeGroupRoleSelector } from './components/AgeGroupRoleSelector'
 import { CriteriaDrawer } from './components/CriteriaDrawer'
 import { EventResultTable } from './components/table/EventResultTable'
+import { EventFilter } from './types/EventFilter'
+
+const FILTER_LS_KEY = 'FILTER_LS_KEY'
 
 export const ResultsPage = () => {
   const resultsMutation = useFetchEventResultsMutation()
@@ -22,10 +25,41 @@ export const ResultsPage = () => {
   const [includeTotal, setIncludeTotal] = useState(true)
 
   useEffect(() => {
-    seasonsMutation.mutate(undefined)
-    resultsMutation.mutate({ categoryIds: [], criterionIds: [], nationalOnly: false, includeTotal: true })
+    const saved = localStorage.getItem(FILTER_LS_KEY)
+    if (!saved) {
+      seasonsMutation.mutate(undefined)
+      resultsMutation.mutate({ categoryIds: [], criterionIds: [], nationalOnly: false, includeTotal: true })
+    } else {
+      const filterData: EventFilter = JSON.parse(saved)
+      seasonsMutation.mutate(filterData.seasonId)
+      resultsMutation.mutate({
+        categoryIds: filterData.categoryIds,
+        criterionIds: filterData.criterionIds,
+        nationalOnly: filterData.nationalOnly,
+        includeTotal: filterData.includeTotal,
+      })
+      setSelectedCategoryIds(filterData.categoryIds)
+      setSelectedCriterionIds(filterData.criterionIds)
+      setNationalOnly(filterData.nationalOnly)
+      setIncludeTotal(filterData.includeTotal)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const saveToLocalStorage = (newValue?: EventFilter) => {
+    localStorage.setItem(
+      FILTER_LS_KEY,
+      JSON.stringify(
+        newValue ?? {
+          seasonId: seasonsMutation.data?.selectedSeason.id,
+          categoryIds: selectedCategoryIds,
+          criterionIds: selectedCriterionIds,
+          nationalOnly: nationalOnly,
+          includeTotal: includeTotal,
+        }
+      )
+    )
+  }
 
   if (resultsMutation.error || seasonsMutation.error) {
     console.error(resultsMutation.error)
@@ -34,18 +68,25 @@ export const ResultsPage = () => {
   }
 
   const sendResultRequest = (national?: boolean) => {
-    resultsMutation.mutate({
+    const newValue = {
       seasonId: selectedSeasonId,
       categoryIds: selectedCategoryIds,
       criterionIds: selectedCriterionIds,
       nationalOnly: national ?? nationalOnly,
       includeTotal,
-    })
+    }
+    saveToLocalStorage(newValue)
+    resultsMutation.mutate(newValue)
   }
 
   const selectedSeasonChange = (event: ChangeEvent) => {
     const newSeasonId = parseInt((event.target as HTMLInputElement).value)
     setSelectedSeasonId(newSeasonId)
+    setSelectedCategoryIds([])
+    setSelectedCriterionIds([])
+    setIncludeTotal(true)
+    saveToLocalStorage({ seasonId: newSeasonId, categoryIds: [], criterionIds: [], includeTotal: true, nationalOnly: false })
+
     seasonsMutation.mutate(newSeasonId)
     resultsMutation.mutate({
       seasonId: newSeasonId,
@@ -54,9 +95,6 @@ export const ResultsPage = () => {
       nationalOnly: nationalOnly,
       includeTotal: true,
     })
-    setSelectedCategoryIds([])
-    setSelectedCriterionIds([])
-    setIncludeTotal(true)
   }
 
   const ageGroupOrRoleSelectionChanged = (ageGroups: AgeGroup[], roles: RatingRole[]) => {
