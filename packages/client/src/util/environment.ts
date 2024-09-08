9ebf673afbce60e28c9658b6730366f23401b9e1
Sámuel Fekete <feketesamu@gmail.com>
2024-09-08 21:18:15 +0200

    configurable mtfsz api url

--- packages/client/src/util/environment.ts
+++ packages/client/src/util/environment.ts
@@ -1,5 +1,6 @@
 export const FUNC_HOST = import.meta.env.VITE_FUNC_HOST || 'http://localhost:3000'
 export const APIM_HOST = import.meta.env.VITE_APIM_HOST || 'https://pontozoapim.azure-api.net'
+export const MTFSZ_API_URL = import.meta.env.VITE_MTFSZ_API_URL
 export const APIM_KEY = import.meta.env.VITE_APIM_KEY
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
 export const APP_INSIGHTS_CONN_STR = import.meta.env.VITE_APP_INSIGHTS_CONN_STR
--- packages/client/src/util/onLoginClick.ts
+++ packages/client/src/util/onLoginClick.ts
@@ -1,5 +1,5 @@
-import { CLIENT_ID, FUNC_HOST } from './environment'
+import { CLIENT_ID, FUNC_HOST, MTFSZ_API_URL } from './environment'
 
 export const onLoginClick = () => {
-  window.location.href = `https://api.mtfsz.hu/oauth/v2/auth?client_id=${CLIENT_ID}&scope=&redirect_uri=${FUNC_HOST}/auth/callback&response_type=code`
+  window.location.href = `${MTFSZ_API_URL}/oauth/v2/auth?client_id=${CLIENT_ID}&scope=&redirect_uri=${FUNC_HOST}/auth/callback&response_type=code`
 }
