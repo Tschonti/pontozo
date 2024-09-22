a11ca39d7b42012542ff7881437270edb63cfa2a
Sámuel Fekete <feketesamu@gmail.com>
2024-09-22 10:46:56 +0200

    more updates

--- packages/client/src/util/environment.ts
+++ packages/client/src/util/environment.ts
@@ -1,5 +1,5 @@
 export const FUNC_HOST = import.meta.env.VITE_FUNC_HOST || 'http://localhost:3000'
-export const APIM_HOST = import.meta.env.VITE_APIM_HOST || 'https://pontozoapim.azure-api.net'
+export const APIM_HOST = import.meta.env.VITE_APIM_HOST || 'https://pontozo-apim.azure-api.net'
 export const MTFSZ_API_URL = import.meta.env.VITE_MTFSZ_API_URL
 export const APIM_KEY = import.meta.env.VITE_APIM_KEY
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
